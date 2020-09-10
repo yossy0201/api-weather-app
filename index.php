f3048a624543c1de17ec8b5acf46f3715ea37cc6
yossy0201 <yossy0201msk@gmail.com>
2020-09-10 18:46:27 +0900

    title-change

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     
     if (array_key_exists('city', $_GET)) {
         
-        $city = str_replace(' ', '', $_GET['city']);
         
     }
 
@@ -21,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-      <title>Weather Scraper</title>
+      <title>今日の天気を見てみよう</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
       
@@ -68,13 +67,13 @@
     
       <div class="container">
       
-          <h1>What's The Weather?</h1>
+          <h2>今日の天気を見てみよう</h2>
           
           
           
           <form>
   <fieldset class="form-group">
-    <label for="city">Enter the name of a city.</label>
+    <label for="city">検索したい住所を入れてください.</label>
     <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo" value = "<?php 
 																										   
 																										   if (array_key_exists('city', $_GET)) {
@@ -86,14 +85,15 @@
 																										   ?>">
   </fieldset>
   
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Serch!</button>
 </form>
       
           <div id="weather"><?php 
               
               if ($weather) {
                   
-                  echo '<div class="alert alert-success" role="alert">
+                  echo '<div class="alert alert-success"
+                  role="alert">
   '.$weather.'
 </div>';
                   
